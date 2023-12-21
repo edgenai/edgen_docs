a99f4fa338c58b1dda34143a9238715cccae9814
francis2tm <franciscomelot@hotmail.com>
2023-12-21 18:37:33 +0000

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/github-pages',
+  basePath: '/edgen_docs',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
