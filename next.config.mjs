df230dfdf4d59d36595cbd18c0a4abe60ed0acff
francis2tm <franciscomelot@hotmail.com>
2024-01-03 19:53:53 -0500

    chore: remove basepath from next config

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/edgen_docs',
+  basePath: '',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
